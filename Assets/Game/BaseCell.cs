9cef8c3b27cb412a87f338130b2185bcbf0199d4
vpuhoff <vpuhoff@live.ru>
2016-09-17 20:33:03 +1000

    Все переписано на 2D

---
+++ Assets/Game/BaseCell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BaseCell : MonoBehaviour {
+    public SpriteRenderer  Render;
+    public List<Sprite> Sprites = new List<Sprite>();
+    // Use this for initialization
+    void Start () {
+        Random rnd = new Random();
+        Render.sprite  = Sprites[Random.Range(0, Sprites.Count)];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
